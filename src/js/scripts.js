9009c4b3f491fa993b7c79cb964a0f81dc658f41
Sorokovsky <Sorokovskys@ukr.net>
2022-07-14 20:04:20 +0300

    tour

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,7 +3,7 @@ const menu = document.querySelector(".header__menu");
 const header = document.querySelector("header");
 if (header){
     document.addEventListener("scroll", () => {
-        if (window.scrollY > 100) {
+        if (window.scrollY > 130) {
             header.style.backgroundColor = "#1a4ed0"
         }else{
             header.style.backgroundColor = "transparent"
@@ -11,7 +11,7 @@ if (header){
     });
 }
 if (burger) {
-    burger.addEventListener("click", (e) => {
+    burger.addEventListener("click", () => {
         burger.classList.toggle("_active");
         menu.classList.toggle("_active");
         document.body.classList.toggle("_lock");
