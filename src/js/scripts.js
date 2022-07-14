4f71d9c38c56141ae62f18ed0bc75dc061c9fe04
Sorokovsky <Sorokovskys@ukr.net>
2022-07-14 15:30:37 +0300

    planss

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,15 @@
 const burger = document.querySelector(".burger");
 const menu = document.querySelector(".header__menu");
+const header = document.querySelector("header");
+if (header){
+    document.addEventListener("scroll", () => {
+        if (window.scrollY > 100) {
+            header.style.backgroundColor = "#1a4ed0"
+        }else{
+            header.style.backgroundColor = "transparent"
+        }
+    });
+}
 if (burger) {
     burger.addEventListener("click", (e) => {
         burger.classList.toggle("_active");
